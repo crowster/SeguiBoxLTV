482b60e6e1d9501d856954967a2543e6c441fdea
inggerman <inggerman@debian-IngGerman.@german>
2016-07-18 22:56:11 -0500

    Se agrega el proyecto SeguriboxLtv

--- seguriboxltvcore/src/main/java/com/seguriboxltv/core/domain/Group.java
+++ seguriboxltvcore/src/main/java/com/seguriboxltv/core/domain/Group.java
@@ -1,165 +1,129 @@
 package com.seguriboxltv.core.domain;
-// Generated Jun 17, 2016 5:59:38 PM by Hibernate Tools 4.3.1.Final
-
 
 import java.math.BigDecimal;
 import java.util.Date;
 
+public class Group {
 
-public class Group{
+    private short groupId;
+    private BigDecimal createdByUserId;
+    private String groupName;
+    private String description;
+    private Boolean isActive;
+    private Date referenceDate;
+    private byte profileType;
+    private boolean preconfigured;
+    private short areaId;
+    private Date deactivatedDate;
+    private Integer deactivatedByUserId;
+    private String profileTypeStr;
 
-	private short groupId;
-	private BigDecimal createdByUserId;
-	private String groupName;
-	private String description;
-	private Boolean isActive;
-	private Date referenceDate;
-	private byte profileType;
-	private boolean preconfigured;
-	private short areaId;
-	private Date deactivatedDate;
-	private Integer deactivatedByUserId;
+    public Group() {
+    }
 
+    public String getProfileTypeStr() {
+        return profileTypeStr;
+    }
 
-	public Group() {
-	}
+    public void setProfileTypeStr() {
+      
 
+        System.out.println("el valor es el siguiente:" );
 
-	public Group(short groupId, BigDecimal createdByUserId, String groupName, String description, Boolean isActive,
-			Date referenceDate, byte profileType, boolean preconfigured, short areaId, Date deactivatedDate,
-			Integer deactivatedByUserId) {
-		super();
-		this.groupId = groupId;
-		this.createdByUserId = createdByUserId;
-		this.groupName = groupName;
-		this.description = description;
-		this.isActive = isActive;
-		this.referenceDate = referenceDate;
-		this.profileType = profileType;
-		this.preconfigured = preconfigured;
-		this.areaId = areaId;
-		this.deactivatedDate = deactivatedDate;
-		this.deactivatedByUserId = deactivatedByUserId;
-	}
+        this.profileTypeStr = "Adminsas";
+    }
 
+    public short getGroupId() {
+        return groupId;
+    }
 
-	public short getGroupId() {
-		return groupId;
-	}
+    public void setGroupId(short groupId) {
+        this.groupId = groupId;
+    }
 
+    public BigDecimal getCreatedByUserId() {
+        return createdByUserId;
+    }
 
-	public void setGroupId(short groupId) {
-		this.groupId = groupId;
-	}
+    public void setCreatedByUserId(BigDecimal createdByUserId) {
+        this.createdByUserId = createdByUserId;
+    }
 
+    public String getGroupName() {
+        return groupName;
+    }
 
-	public BigDecimal getCreatedByUserId() {
-		return createdByUserId;
-	}
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
 
+    public String getDescription() {
+        return description;
+    }
 
-	public void setCreatedByUserId(BigDecimal createdByUserId) {
-		this.createdByUserId = createdByUserId;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public Boolean getIsActive() {
+        return isActive;
+    }
 
-	public String getGroupName() {
-		return groupName;
-	}
+    public void setIsActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 
+    public Date getReferenceDate() {
+        return referenceDate;
+    }
 
-	public void setGroupName(String groupName) {
-		this.groupName = groupName;
-	}
+    public void setReferenceDate(Date referenceDate) {
+        this.referenceDate = referenceDate;
+    }
 
+    public byte getProfileType() {
+        return profileType;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setProfileType(byte profileType) {
+        this.profileType = profileType;
+    }
 
+    public boolean isPreconfigured() {
+        return preconfigured;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setPreconfigured(boolean preconfigured) {
+        this.preconfigured = preconfigured;
+    }
 
+    public short getAreaId() {
+        return areaId;
+    }
 
-	public Boolean getIsActive() {
-		return isActive;
-	}
+    public void setAreaId(short areaId) {
+        this.areaId = areaId;
+    }
 
+    public Date getDeactivatedDate() {
+        return deactivatedDate;
+    }
 
-	public void setIsActive(Boolean isActive) {
-		this.isActive = isActive;
-	}
+    public void setDeactivatedDate(Date deactivatedDate) {
+        this.deactivatedDate = deactivatedDate;
+    }
 
+    public Integer getDeactivatedByUserId() {
+        return deactivatedByUserId;
+    }
 
-	public Date getReferenceDate() {
-		return referenceDate;
-	}
+    @Override
+    public String toString() {
+        return "Group{" + "groupId=" + groupId + ", createdByUserId=" + createdByUserId + ", groupName=" + groupName + ", description=" + description + ", isActive=" + isActive + ", referenceDate=" + referenceDate + ", profileType=" + profileType + ", preconfigured=" + preconfigured + ", areaId=" + areaId + ", deactivatedDate=" + deactivatedDate + ", deactivatedByUserId=" + deactivatedByUserId + ", profileTypeStr=" + profileTypeStr + '}';
+    }
 
+    public void setDeactivatedByUserId(Integer deactivatedByUserId) {
+        this.deactivatedByUserId = deactivatedByUserId;
+    }
 
-	public void setReferenceDate(Date referenceDate) {
-		this.referenceDate = referenceDate;
-	}
-
-
-	public byte getProfileType() {
-		return profileType;
-	}
-
-
-	public void setProfileType(byte profileType) {
-		this.profileType = profileType;
-	}
-
-
-	public boolean isPreconfigured() {
-		return preconfigured;
-	}
-
-
-	public void setPreconfigured(boolean preconfigured) {
-		this.preconfigured = preconfigured;
-	}
-
-
-	public short getAreaId() {
-		return areaId;
-	}
-
-
-	public void setAreaId(short areaId) {
-		this.areaId = areaId;
-	}
-
-
-	public Date getDeactivatedDate() {
-		return deactivatedDate;
-	}
-
-
-	public void setDeactivatedDate(Date deactivatedDate) {
-		this.deactivatedDate = deactivatedDate;
-	}
-
-
-	public Integer getDeactivatedByUserId() {
-		return deactivatedByUserId;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Group [groupId=" + groupId + ", createdByUserId=" + createdByUserId + ", groupName=" + groupName
-				+ ", description=" + description + ", isActive=" + isActive + ", referenceDate=" + referenceDate
-				+ ", profileType=" + profileType + ", preconfigured=" + preconfigured + ", areaId=" + areaId
-				+ ", deactivatedDate=" + deactivatedDate + ", deactivatedByUserId=" + deactivatedByUserId + "]";
-	}
-
-
-	public void setDeactivatedByUserId(Integer deactivatedByUserId) {
-		this.deactivatedByUserId = deactivatedByUserId;
-	}
-
-	
 }
